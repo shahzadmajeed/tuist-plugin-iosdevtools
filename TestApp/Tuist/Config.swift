e510186e199049a6df84147df33b6ef9159b73ba
Shahzad Majeed <shahzy57@gmail.com>
2023-08-25 23:06:19 -0400

    Move plugin to top/root level and use remove plugin link

--- TestApp/Tuist/Config.swift
+++ TestApp/Tuist/Config.swift
@@ -2,7 +2,7 @@ import ProjectDescription
 
 let config = Config(
     plugins: [
-        .local(path: .relativeToManifest("../../Plugins/DKBuildTools")),
+        .git(url: "https://github.com/shahzadmajeed/DKBuildTools", tag: "0.3.0")
         .git(url: "https://github.com/tuist/tuist-plugin-lint", tag: "0.3.0")
     ]
 )
